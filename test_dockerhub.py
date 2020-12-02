22e6b7b9732c4db26c08f69a5382120664eebc80
zyy <431041317@qq.com>
2020-12-02 18:02:27 +0800

    test

--- test_dockerhub.py
+++ test_dockerhub.py
@@ -4,6 +4,6 @@ from username import user_name
 
 def test_dockerhub_repo():
     r = requests.get(
-        f"https://hub.docker.com/repositories/docker/{user_name}/simplelayout/"
+        f"https://hub.docker.com/v2/repositories/{user_name}/simplelayout/"
     )
     assert r.status_code == 200, "无法找到镜像仓库！"
